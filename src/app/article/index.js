34b0cc33832dbe2d348869f251bb70f1068afa9c
irnq <sk1m1gm@yandex.ru>
2023-06-14 01:06:44 +0400

    feat: add i18n service

--- src/app/article/index.js
+++ src/app/article/index.js
@@ -22,10 +22,11 @@ function Article() {
   const dispatch = useDispatch();
   // Параметры из пути /articles/:id
   const params = useParams();
+  const { t, lang } = useTranslate();
   useInit(() => {
     //store.actions.article.load(params.id);
     dispatch(articleActions.load(params.id));
-  }, [params.id]);
+  }, [params.id, lang]);
   const select = useSelectorRedux(
     (state) => ({
       article: state.article.data,
@@ -33,7 +34,6 @@ function Article() {
     }),
     shallowequal,
   ); // Нужно указать функцию для сравнения свойства объекта, так как хуком вернули объект
-  const { t } = useTranslate();
   const callbacks = {
     // Добавление в корзину
     addToBasket: useCallback((_id) => store.actions.basket.addToBasket(_id), [store]),
